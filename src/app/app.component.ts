0c33eb344b81158149fdb7cba709efe8f8255073
emadbastawisi <emadbastawisi@outlook.com>
2025-01-09 17:43:04 +0200

    add new icons and some logic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
+
+import { CommonModule } from '@angular/common';
 
 interface boardItem {
   id: number;
@@ -18,13 +22,22 @@ interface itemType {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatGridListModule, MatDialogModule, MatButtonModule],
+  imports: [
+    CommonModule, // Import explicitly if needed in other modules
+    RouterOutlet,
+    MatGridListModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatRadioModule,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'crown-pair-match';
   selectedId!: number;
+  selectedMode: string = '1';
 
   @ViewChild('dialogTemplate') dialogTemplate!: TemplateRef<any>;
 
@@ -32,37 +45,78 @@ export class AppComponent {
   dialog = inject(MatDialog);
 
   constructor() {
-    this.generateBoardList();
+    this.loadBoardList();
   }
-
-  boardItems: itemType[] = [
-    { name: 'default', image: 'default.jpg' },
-    { name: 'catnips', image: 'catnips.jpg' },
-    { name: 'panda', image: 'panda.jpg' },
-    { name: 'common', image: 'common.jpg' },
-    { name: 'metallia', image: 'metallia.jpg' },
-    { name: 'wesson', image: 'wesson.jpg' },
-    { name: 'tsukuyomi', image: 'tsukuyomi.jpg' },
-    { name: 'king', image: 'king.jpg' },
-    { name: 'dog', image: 'dog.jpg' },
-    { name: 'dragon', image: 'dragon.jpg' },
-    { name: 'zombie-astronaut', image: 'zombie-astronaut.jpg' },
-    { name: 'zombie-boss', image: 'zombie-boss.jpg' },
-    { name: 'plus-one', image: 'plus-one.jpg' },
-    { name: 'crown1', image: 'crown1.jpg' },
-    { name: 'crown2', image: 'crown2.jpg' },
-    { name: 'crown3', image: 'crown3.jpg' },
-    { name: 'crown4', image: 'crown4.jpg' },
-    { name: 'crown5', image: 'crown5.jpg' },
+  boardItems: itemType[] = [];
+  boardItems1: itemType[] = [
+    { name: 'default1', image: 'default1.png' },
+    { name: 'common', image: 'common.png' },
+    { name: 'tsukuyomi', image: 'tsukuyomi.png' },
+    { name: 'catnips', image: 'catnips.png' },
+    { name: 'king', image: 'king.png' },
+    { name: 'wesson', image: 'wesson.png' },
+    { name: 'panda', image: 'panda.png' },
+    { name: 'metallia', image: 'metallia.png' },
+    { name: 'dog', image: 'dog.png' },
+    { name: 'dragon', image: 'dragon.png' },
+    { name: 'zombie-astronaut', image: 'zombie-astronaut.png' },
+    { name: 'zombie-boss', image: 'zombie-boss.png' },
+    { name: 'plus-one', image: 'plus-one.png' },
+    { name: 'plus-two', image: 'plus-two.png' },
+    { name: 'fruit1', image: 'fruit1.png' },
+    { name: 'fruit2', image: 'fruit2.png' },
+    { name: 'fruit3', image: 'fruit3.png' },
+    { name: 'fruit4', image: 'fruit4.png' },
+  ];
+  boardItems2: itemType[] = [
+    { name: 'default2', image: 'default2.png' },
+    { name: 'common', image: 'common.png' },
+    { name: 'tsukuyomi', image: 'tsukuyomi.png' },
+    { name: 'catnips', image: 'catnips.png' },
+    { name: 'king', image: 'king.png' },
+    { name: 'wesson', image: 'wesson.png' },
+    { name: 'panda', image: 'panda.png' },
+    { name: 'metallia', image: 'metallia.png' },
+    { name: 'dog', image: 'dog.png' },
+    { name: 'dragon', image: 'dragon.png' },
+    { name: 'zombie-astronaut', image: 'zombie-astronaut.png' },
+    { name: 'zombie-boss', image: 'zombie-boss.png' },
+    { name: 'plus-one', image: 'plus-one.png' },
+    { name: 'plus-two', image: 'plus-two.png' },
+    { name: 'fruit1', image: 'fruit1.png' },
+    { name: 'fruit2', image: 'fruit2.png' },
+    { name: 'fruit3', image: 'fruit3.png' },
+    { name: 'fruit4', image: 'fruit4.png' },
   ];
   boardList: boardItem[] = [];
+  boardList1: boardItem[] = [];
+  boardList2: boardItem[] = [];
 
+  loadBoardList() {
+    this.onLoadFromLocalStorage();
+    if (this.boardList.length == 0 || this.boardList == null) {
+      this.generateBoardList();
+    }
+  }
   generateBoardList(): void {
-    for (let i = 0; i < 25; i++) {
-      this.boardList.push({
-        id: i,
-        type: { name: 'default', image: 'default.jpg' },
-      });
+    if (this.selectedMode === '1') {
+      for (let i = 0; i < 25; i++) {
+        this.boardList1.push({
+          id: i,
+          type: { name: 'default1', image: 'default1.png' },
+        });
+      }
+      this.boardList = this.boardList1;
+      this.boardItems = this.boardItems1;
+    } else {
+      for (let i = 0; i < 25; i++) {
+        this.boardList2.push({
+          id: i,
+          type: { name: 'default2', image: 'default2.png' },
+        });
+      }
+      this.boardList = this.boardList2;
+      this.boardItems = this.boardItems2;
     }
   }
   onCardClicked(item: boardItem) {
@@ -71,19 +125,57 @@ export class AppComponent {
   }
 
   onCardSelected(item: itemType) {
-    this.boardList[this.selectedId].type = item;
+    if (this.selectedMode == '1') {
+      this.boardList1[this.selectedId].type = item;
+      this.boardList = this.boardList1;
+    } else {
+      this.boardList2[this.selectedId].type = item;
+      this.boardList = this.boardList2;
+    }
     this.dialogRef.close();
+    this.SaveToLocalStorage();
   }
 
   onResetBoard() {
-    this.boardList.forEach((item) => {
-      item.type = { name: 'default', image: 'default.jpg' };
-    });
+    if (this.selectedMode == '1') {
+      this.boardList1.forEach((item) => {
+        item.type = { name: 'default1', image: 'default1.png' };
+      });
+      this.boardList = this.boardList1;
+    } else {
+      this.boardList2.forEach((item) => {
+        item.type = { name: 'default2', image: 'default2.png' };
+      });
+      this.boardList = this.boardList2;
+    }
+    this.SaveToLocalStorage();
   }
-  onSaveBoard() {
-    localStorage.setItem('boardList', JSON.stringify(this.boardList));
+
+  onSelectNoramlMode() {
+    this.selectedMode = '1';
+    this.loadBoardList();
   }
-  onLoadBoard() {
-    this.boardList = JSON.parse(localStorage.getItem('boardList') || '');
+  onSelectFruitMode() {
+    this.selectedMode = '2';
+    this.loadBoardList();
+  }
+
+  SaveToLocalStorage() {
+    if (this.selectedMode == '1') {
+      localStorage.setItem('boardList1', JSON.stringify(this.boardList1));
+    } else {
+      localStorage.setItem('boardList2', JSON.stringify(this.boardList2));
+    }
+  }
+  onLoadFromLocalStorage() {
+    if (this.selectedMode == '1') {
+      this.boardList1 = JSON.parse(localStorage.getItem('boardList1') || '');
+      this.boardList = this.boardList1;
+      this.boardItems = this.boardItems1;
+    } else {
+      this.boardList2 = JSON.parse(localStorage.getItem('boardList2') || '');
+      this.boardList = this.boardList2;
+      this.boardItems = this.boardItems2;
+    }
   }
 }
